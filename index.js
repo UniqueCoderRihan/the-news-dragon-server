4db15182bf62e76a9be851b36d08b98c4b12b6c4
Rihanul Islam <rihanthedev@gmail.com>
2023-04-26 15:05:31 +0600

    Failed To Fetch Data Resolved

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const catagoris = require('./datas/categories.json')
 const news = require('./datas/news.json')
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('The-dragon-server is Running')
