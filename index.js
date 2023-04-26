ec298f330ea4b74c82f09806aba8692fc0ba4691
Rihanul Islam <rihanthedev@gmail.com>
2023-04-26 14:47:56 +0600

    Data Updated

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const catagoris = require('./datas/categories.json')
+const news = require('./datas/news.json')
+
 app.get('/',(req,res)=>{
     res.send('The-dragon-server is Running')
+})
+
+app.get('/catagoris',(req,res)=>{
+    res.send(catagoris)
+})
+
+app.get('/news',(req,res)=>{
+    res.send(news)
 })
 
 app.listen(port,()=>{
