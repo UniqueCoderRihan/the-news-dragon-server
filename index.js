e59d85ef9a77d3ce284e802de2bbd7417d8f0132
Rihanul Islam <rihanthedev@gmail.com>
2023-04-27 09:53:25 +0600

    Data Ready For Client side

--- index.js
+++ index.js
@@ -15,9 +15,26 @@ app.get('/',(req,res)=>{
 app.get('/catagoris',(req,res)=>{
     res.send(catagoris)
 })
-
+// 1. All News Send Tips
 app.get('/news',(req,res)=>{
     res.send(news)
+})
+
+// 2. Specific One Items By Using Id
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    const SelectedNews = news.find(n => n._id===id);
+    // console.log(SelectedNews); 
+    res.send(SelectedNews)
+})
+
+// 3.Categories Way News 
+
+app.get('/catagoris/:id',(req,res)=>{
+    const id = req.params.id;
+    // console.log(id);
+    const categoriesNews = news.filter(n=>n.category_id === id);
+    res.send(categoriesNews)
 })
 
 app.listen(port,()=>{
